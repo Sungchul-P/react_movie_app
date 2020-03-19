a626db47018154baf8988e0676b3ed26e325f1ca
sungchul-dev <devnori@bigin.io>
2020-03-20 02:31:04 +0900

    Updates

---
+++ precache-manifest.b2191954cc252144fe68ecc8ffae03f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b025439f48befb445c5b4f02b62350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49dc4dd65815134b9d95",
+    "url": "/static/css/main.9689ddda.chunk.css"
+  },
+  {
+    "revision": "7010f703a64b3ff19a45",
+    "url": "/static/js/2.0ee51848.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0ee51848.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49dc4dd65815134b9d95",
+    "url": "/static/js/main.965eea94.chunk.js"
+  },
+  {
+    "revision": "93dc36485b9e750f9f14",
+    "url": "/static/js/runtime-main.26245211.js"
+  }
+]);
